c5b15d01cc1f320ae06086524b37d9d824323e22
SMARTIN33 <soizicmartin17@gmail.com>
2024-05-23 14:40:23 +0200

    Upload all files

---
+++ test_echec.cy.js
@@ -0,0 +1,19 @@
+describe("Formualaire Blackmarket",()=>{
+
+    it('visiter un site',function(){
+        cy.visit('https://preprod.backmarket.fr/fr-fr/register')
+        cy.contains('Tout refuser').click()
+        cy.get('#firstName')
+            .type('Soizic')
+            .should("have.value", "Soizic")
+        cy.get('#lastName')
+            .type('MARTIN')
+            .should("have.value", "Martin")
+        cy.get('#signup-email').type('soizic@gmail.com')
+        cy.get('#signup-password').type('123motdepass')
+        cy.contains('J’accepte de recevoir les meilleurs plan du web et la newsletter par mail.').click()
+        cy.contains("Enchantés !").click()
+
+
+    })
+})
